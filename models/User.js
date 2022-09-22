396057e17e8ebd3566bdc96a34976d32cef93394
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-21 19:24:22 -0700

    fix user schema

--- models/User.js
+++ models/User.js
@@ -1,34 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    hashedPassword: {
+      type: String,
+      required: true
+    },
   },
-  email: {
-    type: String,
-    required: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  projects:[{
-    type: Schema.Types.ObjectId,
-    ref: "Project"
-  }],
-  tasks: [{
-    type: Schema.Types.ObjectId,
-    ref: "Task"
-  }],
-  comments: [{
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }]
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model('User', userSchema);
