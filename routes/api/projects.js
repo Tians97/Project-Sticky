c29a9d2237d671d8a3e28bff5911601844b00845
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-21 20:29:41 -0700

    add project api with get all projects for a user by userId

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,18 +1,37 @@
+// const express = require('express')
+// const {
+//     createProject
+// } = require('../../controllers/projectController')
+// const router = express.Router()
+
+
 const express = require('express')
-const {
-    createProject
-} = require('../../controllers/projectController')
+const { mongoose } = require('mongoose')
+const Project = mongoose.model("Project")
 const router = express.Router()
 
-
-//get all projects
-
+//get all projects for a user by userId
+router.get("/user/:userId", async (req, res, next) => {
+    try {
+        const projects = await Project
+            .find({ creator: req.params.userId })
+            // if creator is stored in object id form, we will need to use the following
+            // .find({ creator: Schema.Types.ObjectId(req.params.userId) })
+            .sort({ deadline: -1 });
+        return res.json(projects)
+    }
+    catch (_err) {
+        const err = new Error(_err.message)
+        err.statusCode = 404;
+        return next(err)
+    }
+})
 
 //get a single project
 
 
 //create a new project
-router.post('/', createProject)
+// router.post('/', createProject)
 
 
 
